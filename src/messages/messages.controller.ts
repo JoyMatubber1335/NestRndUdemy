169557c78226e3f8f2eda15354dca164e893bb7d
JoyMatubber1335 <joy.matubber@brainstation-23.com>
2024-07-05 14:44:23 +0600

    Added validation for create messages

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,18 @@
-import { Controller,Get,Post, Body,Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessagesDto } from './dtos/create-messages.dto';
 
 @Controller('messages')
 export class MessagesController {
-    @Get()
-    listMessage(){
-        return "list "
-    }
-    @Post()
-    createMessage(@Body() body:any){
-         console.log(body);
-    }
-    @Get("/:id")
-    getMessage(@Param() id:string){
-        console.log(id);
-    }
-
+  @Get()
+  listMessage() {
+    return 'list ';
+  }
+  @Post()
+  createMessage(@Body() body: CreateMessagesDto) {
+    console.log(body);
+  }
+  @Get('/:id')
+  getMessage(@Param() id: string) {
+    console.log(id);
+  }
 }
