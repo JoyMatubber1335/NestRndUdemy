e0fb1ad36894348d06b32cdd5d8e3acb221d17a6
JoyMatubber1335 <joy.matubber@brainstation-23.com>
2024-07-10 18:04:14 +0600

    Complete service repository

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,32 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+  Injectable,
+} from '@nestjs/common';
 import { CreateMessagesDto } from './dtos/create-messages.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+
   @Get()
   listMessage() {
-    return 'list ';
+    return this.messagesService.findAll();
   }
   @Post()
   createMessage(@Body() body: CreateMessagesDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
   @Get('/:id')
-  getMessage(@Param() id: string) {
-    console.log(id);
+  async getMessage(@Param() id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
   }
 }
